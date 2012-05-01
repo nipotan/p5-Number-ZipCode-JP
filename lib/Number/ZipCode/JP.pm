1cdaa7444379bdbca5a625eb87c211e78288ec10
nipotan <nipotan@nipotan.org>
2012-05-01 12:47:55 +0900

    table update

--- lib/Number/ZipCode/JP.pm
+++ lib/Number/ZipCode/JP.pm
@@ -6,7 +6,7 @@ use 5.008_001;
 use Carp;
 use UNIVERSAL::require;
 
-our $VERSION = '0.20120330';
+our $VERSION = '0.20120427';
 our %ZIP_TABLE = ();
 
 sub import {
--- lib/Number/ZipCode/JP/Table.pm
+++ lib/Number/ZipCode/JP/Table.pm
@@ -3,7 +3,7 @@ package Number::ZipCode::JP::Table;
 use strict;
 use warnings;
 
-our $VERSION = '0.20120330';
+our $VERSION = '0.20120427';
 
 no warnings 'once';
 
