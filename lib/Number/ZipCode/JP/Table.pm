fd1be736685a09b403f74eab3f210e1354b2c159
nipotan <nipotan@gmail.com>
2023-04-28 17:32:41 +0900

    table update

--- lib/Number/ZipCode/JP/Table.pm
+++ lib/Number/ZipCode/JP/Table.pm
@@ -3,7 +3,7 @@ package Number::ZipCode::JP::Table;
 use strict;
 use warnings;
 
-our $VERSION = '0.20230331';
+our $VERSION = '0.20230428';
 
 no warnings 'once';
 
--- lib/Number/ZipCode/JP/Table/Company.pm
+++ lib/Number/ZipCode/JP/Table/Company.pm
@@ -3,9 +3,9 @@ package Number::ZipCode::JP::Table::Company;
 use strict;
 use warnings;
 
-our $VERSION = '0.20230331';
+our $VERSION = '0.20230428';
 
-# Table last modified: 2023-03-31
+# Table last modified: 2023-04-28
 our %ZIP_TABLE = (
     '001' => '(?:8(?:5(?:[0-2]1|55|77|88)|612))',
     '002' => '(?:850[1-4])',
